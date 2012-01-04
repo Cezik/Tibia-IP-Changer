f3f05af6cb534b237d24824ae73f558564c586c5
czepek <czepek@8d1efb19-583e-45ad-ab19-74c306448c61>
2012-01-04 23:23:55 +0000

    # Fixed address 9.42 # Updated to 9.43 # Small replaces in shorts, longs to uints

--- trunk/IpChanger.h
+++ trunk/IpChanger.h
@@ -21,15 +21,24 @@
 
 #include <string>
 
-#define AMOUNT_OF_LAST_SERVERS 50
 #define MAX_SERVERS_IN_LIST 200
 #define LANGUAGE_STRINGS 73
 #define MAX_AMOUNT_OF_PROTOCOLS 50
 
-const short PortDistance = 100;
-const short IpDistance = 112;
+const int16_t PortDistance = 100;
+const int16_t IpDistance = 112;
 const char OTSERV_RSA_KEY[] = "109120132967399429278860960508995541528237502902798129123468757937266291492576446330739696001110603907230888610072655818825358503429057592827629436413108566029093628212635953836686562675849720620786279431090218017681061521755056710823876476444260558147179707119674283982419152118103759076030616683978566631413";
 
 #define ADDRESSES_CHECK "http://czepekipchanger.svn.sourceforge.net/viewvc/czepekipchanger/trunk/Addresses.xml"
+
+#ifdef _MSC_VER
+typedef signed long long int64_t;
+typedef unsigned long uint32_t;
+typedef signed long int32_t;
+typedef unsigned short uint16_t;
+typedef signed short int16_t;
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+#endif
 
 #endif
